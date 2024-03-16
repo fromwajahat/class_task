e81f09014be7bc824741dad21386695501e9a303
wajahat mazhar <fromwajahat@gmail.com>
2024-03-16 09:09:59 +0500

    Chapter2

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
-alert("first name")
-alert("last name")
-alert("E-mail")
-alert("Phone Number")
-alert("Password")
+
+// Chapter 1
+//Task 1
+
+// alert("first name")
+// alert("last name")
+// alert("E-mail")
+// alert("Phone Number")
+// alert("Password")
+
+//Task 2
+// alert("You're learning JavaScript");
+//Task 3
+// alert("JavaScript");
+
+
+// Chapter 2
+//1. Declare any variable in the camelCase format.
+//2. Declare a variable of your choice without defining it. Then, in a second statement, assign it a string of your choice.
+//3. Declare the variable teamName and Alert your Team name.
+//4. This statement has already been coded. var bestMan = "Charlie"; Assign the variable a new string.
+
+var teamName = "SMIT Team";
+
+var Charlie;
+Charlie = "Nouman";
+alert(teamName);
+
+var bestMan = "Charlie";
+bestMan = "Nouman";
