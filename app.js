3deaaf862833741b0f5991bf56600394063de8e5
wajahat mazhar <fromwajahat@gmail.com>
2024-03-16 09:24:18 +0500

    Chapter 3 (Variables for numbers)

--- app.js
+++ app.js
@@ -20,11 +20,42 @@
 //3. Declare the variable teamName and Alert your Team name.
 //4. This statement has already been coded. var bestMan = "Charlie"; Assign the variable a new string.
 
-var teamName = "SMIT Team";
+// var teamName = "SMIT Team";
 
-var Charlie;
-Charlie = "Nouman";
-alert(teamName);
+// var Charlie;
+// Charlie = "Nouman";
+// alert(teamName);
 
-var bestMan = "Charlie";
-bestMan = "Nouman";
+// var bestMan = "Charlie";
+// bestMan = "Nouman";
+
+
+//     Chapter 3 (Variables for numbers)
+
+
+// 1. Declare a variable “caseQty”
+// 2. Assign to the variable caseQty, which has already been declared, the value 144.
+// 3. Rewrite this statement so the variable can be used in a math operation. var num = "9";
+// 4. In one statement declare a variable. In a second statement assign it the sum of 2 numbers.
+// 5. What is the value of orderTotal? var merchTotal = 100; var shippingCharge = 10; var orderTotal = merchTotal + shippingCharge; Try it yourself.
+// 6. In the first statement declare a variable and assign it a number. In the second statement, change the value of the variable by adding it together with a number.
+
+
+
+var caseQty;
+caseQty = 144;
+var num = 9;
+var sumOfNum;
+sumOfNum = caseQty + num;
+console.log(sumOfNum);
+
+
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal);
+
+var num1 = 50;
+num1 = num1+21;
+console.log(num1);
+
