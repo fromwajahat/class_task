1855d3af46f333259e99b6dd754c185dae2d0cef
wajahat mazhar <fromwajahat@gmail.com>
2024-03-18 04:24:40 +0500

    Chapter 6

--- app.js
+++ app.js
@@ -42,20 +42,123 @@
 
 
 
-var caseQty;
-caseQty = 144;
-var num = 9;
-var sumOfNum;
-sumOfNum = caseQty + num;
-console.log(sumOfNum);
+// var caseQty;
+// caseQty = 144;
+// var num = 9;
+// var sumOfNum;
+// sumOfNum = caseQty + num;
+// console.log(sumOfNum);
 
 
-var merchTotal = 100;
-var shippingCharge = 10;
-var orderTotal = merchTotal + shippingCharge;
-console.log(orderTotal);
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// console.log(orderTotal);
 
-var num1 = 50;
-num1 = num1+21;
-console.log(num1);
+// var num1 = 50;
+// num1 = num1+21;
+// console.log(num1);
+
+// =====================Chapter 4 (Variable names Legal and
+//     Illegal)
+//     1. Correct this statement.
+//     var product cost = 3.45;
+//     2. Rewrite this using camelCase.
+//     var Nameofband;
+//     3. In a single statement declare a legally-named variable and assign a
+//     number to it.
+//     4. Declare a variable that is a combination of your first and last
+//     names. Use camelCase.
+//     5. List the legal and Illegal Variables.
+
+// var product_cost = 3.45;
+// var nameOfBand;
+// var nameOfBand1 = 3.6;
+// var firstEndLastName;
+
+// var 123;
+// var name of band; 
+// var first&LastName;
+
+// var num123;
+// var myNumber;
+// var cont_number;
+// var first$lastName;
+
+// ==========================Chapter 5 (Math Expression I)
+// 1. What is the name and symbol of the arithmetic operator that
+// gives you the remainder when one number is divided by another?
+// 2. What is the value of num?
+// var num = 20 % 6;
+// 3. In a single statement, declare the variable largeNum and assign it
+// the result of 1,000 multiplied by 2,000.
+// 4. Assign to a variable the value represented by one variable
+// subtracted from the value represented by another variable
+// 5. Assign to a variable the remainder when one number is divided by
+// another. The variable hasn't been declared beforehand. Make up
+// the variable name.
+// 6. Code an alert that displays the result of a multiplication on 2
+// numbers.
+
+
+// Percentage Operator %
+
+// var num = 20 % 6;
+// console.log(num); // 2
+// var largeNum = 1000 * 2000;
+// var result = largeNum;
+// console.log(result);
+
+// var val_1 = 50;
+// var val_2 = 150;
+// var result_val = val_2 - val_1;
+// console.log(result_val);
+
+// var remainder1 = val_1 % val_2;
+// console.log(remainder1);
+
+// alert(val_1 * val_2);
+
+// ==================Chapter 6 (Math Expression II)
+
+// 1. Code a short form of x = x + 1; Use either of the two legal
+// expressions.
+// 2. If x has a value of 100, what is the fastest way to reduce it to 99
+// with a math expression?
+// 3. var x = 50;
+// var y = x++;
+// What is the value of y?
+// 4. var y = 50;
+// var z = --y;
+// What is the value of z?
+// 5. In a single statement, decrement num and assign its original value
+// to newNum.
+// 6. In a single statement add 1 to a variable and assign its original
+// value to another variable.
+// 7. Assign a number value to a variable. Increment the variable. Display the new value in an alert
+
+    // x++
+// var x = 100
+// x = x-1;
+// var x = 50;
+// var y = x++;
+// console.log(y); // 50
+
+// var y = 50;
+// var z = y--;
+// console.log(z); // 50
+
+// var num = 90;
+// var newNum = --num;
+// console.log(newNum);
+
+// var x = 100;
+// var y = x + 1;
+
+// var numval = 103;
+// ++numval;
+// alert(numval);
+
+
+
 
