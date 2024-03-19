f297321a069b223cb95d50a4e404119bba514bc0
wajahat mazhar <fromwajahat@gmail.com>
2024-03-19 23:37:17 +0500

    Chapter 7 (Math Expression III)

--- app.js
+++ app.js
@@ -159,6 +159,54 @@
 // ++numval;
 // alert(numval);
 
+// ===========================Chapter 7 (Math Expression III)
+// 1. var calculatedNum = 2 + (2 * 6);
+// What is the value of calculatedNum?
+// 2. var calculatedNum = (2 + 2) * 6;
+// What is the value of calculatedNum?
+// 3. var calculatedNum = (2 + 2) * (4 + 2);
+// What is the value of calculatedNum?
+// 4. var calculatedNum = ((2 + 2) * 4) + 2;
+// What is the value of calculatedNum?
+// Note: Try all the above equations yourself.
+// 5. Write a statement that assigns to cost the result of 2 + 2 * 4 + 10,
+// clarified with parentheses, producing 56.
+// 6. Write a statement that assigns to units the result of 2 + 2 * 4 + 10,
+// clarified with parentheses, producing 20.
+// 7. Write a statement that assigns to pressure the result of 4 / 2 * 4,
+// clarified with parentheses, producing 5.
+
+// var calculatedNum = 2 + (2 * 6);
+// console.log(calculatedNum);   // 14
+
+// var calculatedNum = (2 + 2) * 6;
+// console.log(calculatedNum);   // 24
+
+// var calculatedNum = (2 + 2) * (2 + 4);
+// console.log(calculatedNum);   // 24
+
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// console.log(calculatedNum);   // 18
+
+// var calculatedNum = (2 + 2) * (4 + 10);
+// console.log(calculatedNum);   // 56
+
+// var calculatedNum = (2 + 2 * 4 + 10);
+// console.log(calculatedNum);   // 20
+
+// var calculatedNum = 4 / (2 * 4);
+// console.log(calculatedNum);   // 0.5
+
+
+
+
+
+
+
+
+
+
+
 
 
 
